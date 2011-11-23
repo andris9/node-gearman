bdcef77e875fd2446943f2028ce1cafab5ef7ee6
andris9 <andris@node.ee>
2011-11-23 16:34:24 +0200

    updated example

--- examples/streamWorker.js
+++ examples/streamWorker.js
@@ -2,9 +2,8 @@ var Gearman = require("../lib/gearman"),
     fs = require("fs"),
     zlib = require("zlib");
 
-var gearman = new Gearman(); // defaults to localhost
-
-var filepath = __dirname+"/../../wordlist.txt";
+var gearman = new Gearman(), // defaults to localhost
+    filepath = __dirname+"/../../wordlist.txt";
 
 gearman.registerWorker("stream", function(payload, worker){
     // pipe stream to gzip and then to worker
@@ -14,4 +13,4 @@ gearman.registerWorker("stream", function(payload, worker){
 
 gearman.on("connect", function(){
     console.log("Listening for jobs");
-})
+});
